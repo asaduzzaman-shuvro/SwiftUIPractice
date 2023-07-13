58438fe282448db2f55c9ec2d1c6047b2a3408ff
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2023-07-13 23:33:54 +0600

    Badge view added

---
+++ Shared/Views/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUIPractice
+//
+//  Created by Asaduzzaman Shuvro on 13/7/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
